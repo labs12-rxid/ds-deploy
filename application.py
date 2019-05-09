0ba1d054909e46d4bf0427c72b2ac3e52b2e7c15
workingjubilee <workingjubilee@gmail.com>
2019-05-09 14:53:47 -0700

    Preparing to rewrite history.

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 """
 Main application and routing logic
 """
+<<<<<<< HEAD
 from flask import Flask, request, render_template, jsonify
 from joblib import load
 from flask_cors import CORS
@@ -35,19 +36,41 @@ import json
 import html
 from string import punctuation
 from collections import deque
+=======
+# _____ imports _____________
+from flask import Flask, request, render_template, jsonify
+from joblib import load
+from flask_cors import CORS
+import pandas as pd
+import json
+
+# ______ Module imports _____
+from drugscom import drugscom
+from rxid_util import parse_input
+from rds_lib import db_connect, query_sql_data, verify_output
+from rekog import post_rekog
+
+drugs_com = drugscom()
+>>>>>>> 073ad39da74296166c6663ce4eb236fa50451c83
 
 """ create + config Flask app obj """
 application = Flask(__name__)
 CORS(application)
 
+<<<<<<< HEAD
 
 # ______________ R O U T E S  _____________________
 # ________  HOME __________
+=======
+# ______________ R O U T E S  _____________________
+# ________ / HOME __________
+>>>>>>> 073ad39da74296166c6663ce4eb236fa50451c83
 @application.route('/')
 def index():
     return render_template('base.html', title='Home')
 
 # ________  /indentify/  route __________
+<<<<<<< HEAD
 # __ input  image uri, S3 bucket
 @application.route('/identify', methods=['GET', 'POST'])
 def indentify():
@@ -63,11 +86,27 @@ def indentify():
 
 
 # ________  /rxdata/  route __________
+=======
+# __ input  {'imprint' : 'M370',  'color' : 1,  'shape' : 6}    
+@application.route('/identify', methods=['GET', 'POST'])
+def identify():
+    if request.method == 'POST':
+        post_params = request.get_json(force=True)
+        results = get_drugscom(post_params)
+        return results
+    else:
+        return jsonify("GET request to /identify :")
+
+
+# ________  /rxdata/  route __________
+# __ {'imprint' : 'M370',  'color' : 1,  'shape' : 6}    
+>>>>>>> 073ad39da74296166c6663ce4eb236fa50451c83
 @application.route('/rxdata', methods=['GET', 'POST'])
 def rxdata():
     if request.method == 'POST':
         post_params = request.get_json(force=True)
         output_info = query_sql_data(post_params)
+<<<<<<< HEAD
         return jsonify(output_info)
     else:
         return jsonify("YOU just made a GET request to /rxdata")
@@ -82,17 +121,29 @@ def nnet():
     else:
         
         return jsonify("YOU just made a GET request to /nnet")
+=======
+        return output_info
+
+    else:
+        return jsonify("GET request to /rxdata :")
+
+>>>>>>> 073ad39da74296166c6663ce4eb236fa50451c83
 
 # ________  /rekog/  route __________
 @application.route('/rekog', methods=['GET', 'POST'])
 def rekog():
     if request.method == 'POST':
         post_params = request.get_json(force=True)
+<<<<<<< HEAD
+=======
+        # https://s3.amazonaws.com/labs12-rxidstore/reference/00002-3228-30_391E1C80.jpg
+>>>>>>> 073ad39da74296166c6663ce4eb236fa50451c83
         output_info = post_rekog(post_params)
         return jsonify(output_info)
     else:
         return jsonify("YOU just made a GET request to /rekog")
 
+<<<<<<< HEAD
 
 # ___________________ FUNCTIONS ________________________________
 def drugscom():
@@ -213,6 +264,30 @@ def verify_output(pgres_engine):
     for row in pgres_engine.execute(query).fetchall():
         print(row)
     return
+=======
+# ________  /nnet/  route __________
+@application.route('/nnet', methods=['GET', 'POST'])
+def nnet():
+    if request.method == 'POST':
+        post_params = request.get_json(force=True)
+        return jsonify(post_params)
+    else:
+        return jsonify("YOU just made a GET request to /nnet")
+
+
+# ___________________ FUNCTIONS ________________________________
+def get_drugscom(query_string):
+    out_put = ''
+    try:
+        d_data = drugs_com.get_data(query_string)
+        out_put = json.dumps(d_data, indent=4)
+    except Exception as e:
+        out_put = f'error: {e}'
+    finally:
+        if drugs_com is not None: 
+            drugs_com.close()
+    return out_put
+>>>>>>> 073ad39da74296166c6663ce4eb236fa50451c83
 
 
 # __________ M A I N ________________________
@@ -223,9 +298,15 @@ if __name__ == '__main__':
     # application.run(debug=True)
 
     #  --- for terminal debugging ------
+<<<<<<< HEAD
     #input_str = 'RED PILL'
     #print(predict(input_str))
 
+=======
+    # results = get_drugscom()
+    # print(results)
+# __________________________________________________
+>>>>>>> 073ad39da74296166c6663ce4eb236fa50451c83
 # to launch from terminal : 
 #    change line 25 to  application.run(debug=True)
 #    cd to folder (where application.py resides)
