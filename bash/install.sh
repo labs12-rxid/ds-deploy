6316a747a6b375fcef1260430b4fcf3f2077e35a
workingjubilee <workingjubilee@gmail.com>
2019-05-08 20:13:44 -0700

    Shift a directory over.

--- bash/install.sh
+++ bash/install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pip3 install -r /srv/www/flask/requirements.txt
+pip3 install -r /srv/www/pyapp/requirements.txt
--- bash/start.sh
+++ bash/start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd /srv/www/pyapp
 gunicorn -b 0.0.0.0:8000 application -D --forwarded-allow-ips "*"
